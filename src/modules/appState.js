8589d41b0d33d08b05e5484c1ac92f26724bbcd2
Tsukuni <tamkchi.fugu@gmail.com>
2020-04-17 12:35:38 +0900

    reducer定義

--- src/modules/appState.js
+++ src/modules/appState.js
@@ -8,3 +8,30 @@ const RESET_APP_STATE = 'RESET_APP_STATE';
 export const setPlayer = player => ({ type: SET_PLAYER, payload: { player }});
 export const setWinner = winner => ({ type: SET_WINNER, payload: { winner }});
 export const resetAppState = () => ({ type: RESET_APP_STATE });
+
+const INITIAL_STATE = {
+  player: 2,
+  winner: null,
+};
+
+// reducer
+export const reducer = (state=INITIAL_STATE, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case SET_PLAYER:
+      return {
+        ...state,
+        player: payload.player
+      };
+    case SET_WINNER:
+      return {
+        ...state,
+        winner: payload.winner
+      };
+    case RESET_APP_STATE:
+      return INITIAL_STATE;
+    default:
+      return state;
+  }
+}
--- src/modules/position.js
+++ src/modules/position.js
@@ -5,3 +5,33 @@ const RESET_POSITION = 'RESET_POSITION';
 // action
 export const setPosition = (key, value) => ({ type: SET_POSITION, payload: { key, value }});
 export const resetPosition = () => ({ type: RESET_POSITION });
+
+
+const INITIAL_STATE = {
+  topLeft: null,
+  topCenter: null,
+  topRight: null,
+  centerLeft: null,
+  center: null,
+  centerRight: null,
+  bottomLeft: null,
+  bottomCenter: null,
+  bottomRight: null,
+};
+
+// reducer
+export const reducer = (state=INITIAL_STATE, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case SET_POSITION:
+      return {
+        ...state,
+        [payload.key]: payload.value
+      };
+    case RESET_POSITION:
+      return INITIAL_STATE;
+    default:
+      return state;
+  }
+}
