cbf31b64bb2a03a748bb52a47977645532472fa2
Tsukuni <tamkchi.fugu@gmail.com>
2020-04-17 12:34:41 +0900

    action creator定義

---
+++ src/modules/appState.js
@@ -0,0 +1,4 @@
+// action creator
+const SET_PLAYER = 'SET_PLAYER';
+const SET_WINNER = 'SET_WINNER';
+const RESET_APP_STATE = 'RESET_APP_STATE';
---
+++ src/modules/position.js
@@ -0,0 +1,3 @@
+// action creator
+const SET_POSITION = 'SET_POSITION';
+const RESET_POSITION = 'RESET_POSITION';
