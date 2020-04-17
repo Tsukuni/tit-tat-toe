c5c917febf601603662850eb91fe2ac2d511408f
Tsukuni <tamkchi.fugu@gmail.com>
2020-04-17 12:35:16 +0900

    action定義

--- src/modules/appState.js
+++ src/modules/appState.js
@@ -2,3 +2,9 @@
 const SET_PLAYER = 'SET_PLAYER';
 const SET_WINNER = 'SET_WINNER';
 const RESET_APP_STATE = 'RESET_APP_STATE';
+
+
+// action
+export const setPlayer = player => ({ type: SET_PLAYER, payload: { player }});
+export const setWinner = winner => ({ type: SET_WINNER, payload: { winner }});
+export const resetAppState = () => ({ type: RESET_APP_STATE });
--- src/modules/position.js
+++ src/modules/position.js
@@ -1,3 +1,7 @@
 // action creator
 const SET_POSITION = 'SET_POSITION';
 const RESET_POSITION = 'RESET_POSITION';
+
+// action
+export const setPosition = (key, value) => ({ type: SET_POSITION, payload: { key, value }});
+export const resetPosition = () => ({ type: RESET_POSITION });
